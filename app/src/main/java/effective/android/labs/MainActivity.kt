e0092fd1dc8dec2e515067e4d405420d2da79521
RoGad <krokon199@gmail.com>
2024-09-13 17:24:06 +0600

    initial commit

---
+++ app/src/main/java/effective/android/labs/MainActivity.kt
@@ -0,0 +1,20 @@
+package effective.android.labs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import effective.android.labs.presentation.theme.AnroidLabsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AnroidLabsTheme {
+
+            }
+        }
+    }
+}
+
